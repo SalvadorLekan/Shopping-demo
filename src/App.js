771850aa19345bfcb25f74e39088e014a6908937
SalvadorLekan <lekansalvador@gmail.com>
2020-07-24 07:40:47 +0100

    linked the home page to the app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HomePage/>
+    </>
   );
 }
 
