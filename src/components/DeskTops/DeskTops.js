f3af841eca13dc2ef834b5f13327b4b55ee496cd
SalvadorLekan <lekansalvador@gmail.com>
2020-07-24 17:34:45 +0100

    Adjusted Redirect Parameters

--- src/components/DeskTops/DeskTops.js
+++ src/components/DeskTops/DeskTops.js
@@ -3,7 +3,8 @@ import React from 'react'
 function DeskTops() {
     return (
         <div>
-            <h1>Some Desktops</h1>
+            <h1>Page Doesn't exist yet :) </h1>
+            <p>Also redirect is working</p>
         </div>
     )
 }
