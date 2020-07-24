380c56e7ee7bc1ac2466c51db2e3b88c5ec3f69a
SalvadorLekan <lekansalvador@gmail.com>
2020-07-24 17:40:19 +0100

    minor fix

--- src/components/DeskTops/DeskTops.js
+++ src/components/DeskTops/DeskTops.js
@@ -3,7 +3,7 @@ import './DeskTop.scss'
 
 function DeskTops() {
     return (
-        <div>
+        <div className='text-center'>
             <h1>Page Doesn't exist yet :) </h1>
             <p>Also redirect is working</p>
         </div>
