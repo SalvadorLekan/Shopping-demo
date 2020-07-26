21ab8a4be10b5b0e356175b17008017e9f09ec96
SalvadorLekan <lekansalvador@gmail.com>
2020-07-26 10:28:51 +0100

    made edits to the styling

--- src/components/form-input/FormInput.js
+++ src/components/form-input/FormInput.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './FormInput.scss'
 
-function FormInput({name, svalue, changevalue, children, type}) {
+function FormInput({name, svalue, changevalue, children, type, required}) {
     return (
         <label className='form-label'>
             {children}
-            <input name={name} value={svalue} onChange={e=>changevalue(e.target.value)} type={type}/>
+            <input name={name} value={svalue} onChange={e=>changevalue(e.target.value)} type={type} required={required&&required}/>
         </label>
     )
 }
--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,15 +1,17 @@
 import React,{useState} from 'react'
 import CostumForm from '../CostumForm/CostumForm'
 import FormInput from '../form-input/FormInput'
+import Btn from '../btn/Btn'
 
 function SignIn() {
     const [value, setvalue] = useState('')
     return (
         <div>
-            <CostumForm>
-                <FormInput name='text' type='text' svalue={value} changevalue={setvalue}>Name</FormInput>
+            <CostumForm submitFunction={()=>console.log('object')}>
+                <FormInput name='text' type='text' svalue={value} changevalue={setvalue} required>Name</FormInput>
                 <FormInput name='password' type='password' svalue={value} changevalue={setvalue}>Password</FormInput>
                 <FormInput name='confirmpassword' type='password' svalue={value} changevalue={setvalue}>Confirm Password</FormInput>
+                <Btn>Sign Up</Btn>
             </CostumForm>
         </div>
     )
