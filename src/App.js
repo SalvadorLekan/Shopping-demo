812a1f5a01a71c955c14b44c22adac500b1125a6
SalvadorLekan <lekansalvador@gmail.com>
2020-07-26 08:44:29 +0100

    Added signin page as a route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './components/HomePage/HomePage';
 import { Switch,Route } from 'react-router-dom'
 import DeskTops from './components/DeskTops/DeskTops';
 import Shop from './components/Shop/Shop';
+import SignIn from './components/sign-in/SignIn';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={Shop}></Route>
+        <Route exact path='signin' component={SignIn}></Route>
       <Route exact path='/:DeskTops' component={DeskTops}></Route>
       </Switch>
       
