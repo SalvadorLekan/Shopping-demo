962c3db79bb4eb0c83a1c6c0375c31e7b4b2e31a
SalvadorLekan <lekansalvador@gmail.com>
2020-07-26 18:33:36 +0100

    fayabayse

---
+++ src/firebase/FireBaseUtil.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const config={
+    apiKey: "AIzaSyD3sp-MwQxrksYnZhZ9VaFFhhIIf4tQDPY",
+    authDomain: "salva-shopping.firebaseapp.com",
+    databaseURL: "https://salva-shopping.firebaseio.com",
+    projectId: "salva-shopping",
+    storageBucket: "salva-shopping.appspot.com",
+    messagingSenderId: "612241201711",
+    appId: "1:612241201711:web:7e8fe8eb0567e080ae84da",
+    measurementId: "G-L4BPT43C71"
+  };
+  firebase.initializeApp(config);
