7bcada41cc2f75523625cca9ea0492e501c436e5
SalvadorLekan <lekansalvador@gmail.com>
2020-07-26 09:56:08 +0100

    Added reuseable button components

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -7,7 +7,9 @@ function SignIn() {
     return (
         <div>
             <CostumForm>
-                <FormInput name='text' type='text' svalue={value} changevalue={setvalue}>Test</FormInput>
+                <FormInput name='text' type='text' svalue={value} changevalue={setvalue}>Name</FormInput>
+                <FormInput name='password' type='password' svalue={value} changevalue={setvalue}>Password</FormInput>
+                <FormInput name='confirmpassword' type='password' svalue={value} changevalue={setvalue}>Confirm Password</FormInput>
             </CostumForm>
         </div>
     )
