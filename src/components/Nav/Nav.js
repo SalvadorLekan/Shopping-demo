9fa569334cef18f81b73c19658554c0369658414
SalvadorLekan <lekansalvador@gmail.com>
2020-07-28 21:40:20 +0100

    Revamped the  nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Nav.scss'
 import { Link } from 'react-router-dom'
 
 function Nav({userstate}) {
+    const [ClassName, setClassName] = useState('')
+    const handletoggle=(e) =>{
+        ClassName? setClassName(''):setClassName('show_list')
+    
+    };
     return (
         <nav className="nav">
         <div className="container">
             <div className="logo">
                 <Link to='/'>Salva</Link>
             </div>
-            <div id="mainListDiv" className="main_list">
+            <span className="navTrigger" onClick={handletoggle}>
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+            <div id="mainListDiv" className={`main_list ${ClassName}`}>
                 <ul className="navlinks">
                     <li><Link to='/shop'>Shop</Link></li>
                     <li><Link to='/'>Portfolio</Link></li>
@@ -20,11 +30,6 @@ function Nav({userstate}) {
                             <Link to='/signin'>Sign In</Link>}</li>
                 </ul>
             </div>
-            <span className="navTrigger">
-                <i></i>
-                <i></i>
-                <i></i>
-            </span>
         </div>
     </nav>
     )
