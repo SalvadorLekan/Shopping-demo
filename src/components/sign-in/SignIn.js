b97706e31347c861019b27b61465c8bb7bfd4567
SalvadorLekan <lekansalvador@gmail.com>
2020-07-29 16:11:03 +0100

    Completed Signing in

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -3,27 +3,58 @@ import CostumForm from '../CostumForm/CostumForm'
 import FormInput from '../form-input/FormInput'
 import './Signin.scss'
 import Btn from '../btn/Btn'
-import { siginwithGoogle } from '../../firebase/FireBaseUtil'
+import { siginwithGoogle, auth, createUserProfile } from '../../firebase/FireBaseUtil'
 
 function SignIn() {
-    const [value, setvalue] = useState('')
+    const [displayName, setdisplayName] = useState('')
     const [mail, setmail] = useState('')
     const [password, setpassword] = useState('')
     const [inmail, setinmail] = useState('')
     const [inpassword, setinpassword] = useState('')
     const [confirm, setconfirm] = useState('')
+
+    const signupSubmit=async ()=>{
+        if (password!==confirm) {
+            alert("password don't match")
+            return
+        }
+        try {
+            const {user} = await auth.createUserWithEmailAndPassword(mail,password)
+            createUserProfile(user, {displayName})
+            setdisplayName('')
+            setmail('')
+            setpassword('')
+            setconfirm('')
+        } catch (error) {
+            console.log(error.message)
+        }
+        
+    }
+    const signinSubmit=async()=>{
+
+        try {
+            await auth.signInWithEmailAndPassword(inmail,inpassword)
+            setinmail('')
+            setinpassword('')
+        } catch (error) {
+            console.log(error.message)
+        }
+       
+
+    }
+
     return (
         <div className='signin'>
-            <CostumForm submitFunction={()=>console.log('object')}>
+            <CostumForm submitFunction={signinSubmit}>
                 <h3 className='h3'>Sign In</h3>
             <FormInput name='mail' type='email' svalue={inmail} changevalue={setinmail} required>Email</FormInput>
                 <FormInput name='password' type='password' svalue={inpassword} changevalue={setinpassword} required>Password</FormInput>
                 <div className='wrap-flex'><Btn type='submit'>Sign In</Btn>
                 <Btn type='button'click={siginwithGoogle}>Google Sign In</Btn></div>
             </CostumForm>
-            <CostumForm submitFunction={()=>console.log('object')}>
+            <CostumForm submitFunction={signupSubmit}>
                 <h3 className='h3'>Sign Up</h3>
-                <FormInput name='text' type='text' svalue={value} changevalue={setvalue} required>Name</FormInput>
+                <FormInput name='text' type='text' svalue={displayName} changevalue={setdisplayName} required>Name</FormInput>
                 <FormInput name='mail' type='email' svalue={mail} changevalue={setmail} required>Email</FormInput>
                 <FormInput name='password' type='password' svalue={password} changevalue={setpassword} required>Password</FormInput>
                 <FormInput name='confirmpassword' type='password' svalue={confirm} changevalue={setconfirm} required>Confirm Password</FormInput>
